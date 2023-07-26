d4da491f04e41c07378ecb6c408791cba4e59b6b
Jarvy <jarvy@physiotec.org>
2023-07-26 18:05:21 +0000

    add code in files types: .php,.js remove code in files types: .php

--- content.js
+++ content.js
@@ -1,2 +1,3 @@
 console.log("scinw")
 console.log("chxlx")
+console.log("tuwsx")
--- content.php
+++ content.php
@@ -146,5 +146,5 @@
         echo "cbydk";
         echo "pvbfx";
         echo "jpcik";
-        echo "hmcip";
+        echo "pgwam";
 ?>
